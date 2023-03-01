d7424c962dbe89cc6623b3d2cbf3ff6151811bdf
Ivan <ivandanilov282@gmail.com>
2023-03-01 15:30:19 +0100

    Code refactoring

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 function addActiveClass(id) {
     const clickedElement = document.getElementById(id);
-
     const elements = document.getElementsByClassName('accordion');
 
     for (let i = 0; i < elements.length; i++) {
         if (elements[i] !== clickedElement) {
             let classNames = elements[i].classList.value;
+
             if (classNames.includes('active')) {
                 elements[i].classList.remove('active');
+            }
+
+            let imgClassNames = elements[i].children[0].children[1].classList;
+
+            if (imgClassNames.value.includes('rotate-image')) {
+                imgClassNames.remove('rotate-image');
             }
         }
         else {
             if (!clickedElement.classList.contains('active')) {
                 clickedElement.classList.add('active');
+                clickedElement.children[0].children[1].classList.add('rotate-image');
             }
             else {
                 clickedElement.classList.remove('active');
+                clickedElement.children[0].children[1].classList.remove('rotate-image');
             }
         }
     }
